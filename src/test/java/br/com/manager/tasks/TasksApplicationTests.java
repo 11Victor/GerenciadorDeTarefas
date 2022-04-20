d55b6b0e31a71eb9dc48fa61dda5dffb9c5e80a0
11Victor <victor.alcantara@unionitdigital.com.br>
2022-04-20 19:52:30 -0300

    Criação do projeto e atualização do pom.xml

---
+++ src/test/java/br/com/manager/tasks/TasksApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.manager.tasks;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
