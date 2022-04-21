9bf37ce4b56ced9499d3e9f7610303d793ea8420
11Victor <victor.alcantara@unionitdigital.com.br>
2022-04-20 22:31:05 -0300

    Requisições HTTP Pessoa

--- src/main/java/br/com/manager/tasks/service/PessoaService.java
+++ src/main/java/br/com/manager/tasks/service/PessoaService.java
@@ -1,9 +1,11 @@
 package br.com.manager.tasks.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.manager.tasks.model.Pessoa;
 import br.com.manager.tasks.repository.PessoaRepository;
@@ -14,9 +16,24 @@ public class PessoaService {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 
+	// Cadastrar nova pessoa
+	public Optional<Pessoa> postPessoa(@RequestBody Pessoa pessoa) {
+		return Optional.of(pessoaRepository.save(pessoa));
+	}
+
+	// Editar pessoa
+	public Optional<Pessoa> putPessoa(@RequestBody Pessoa pessoa) {
+		return Optional.of(pessoaRepository.save(pessoa));
+	}
+
 	// Buscar todas pessoas
-	public List<Pessoa> BuscarPessoas() {
+	public List<Pessoa> findAll() {
 		return pessoaRepository.findAll();
+	}
+
+	// Buscar por ID
+	public Optional<Pessoa> findById(long id) {
+		return pessoaRepository.findById(id);
 	}
 
 }
